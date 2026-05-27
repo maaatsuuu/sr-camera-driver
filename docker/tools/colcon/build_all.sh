7d0c98e4851eea11793c4fc3f31b73f47aa32bf9
matsudri <matudariku@gmail.com>
2026-05-27 16:28:08 +0900

    [fix]Livox用の設定を削除

--- docker/tools/colcon/build_all.sh
+++ docker/tools/colcon/build_all.sh
@@ -2,18 +2,13 @@
 
 export CCACHE_DIR=${HOME}/.ccache
 export CCACHE_TEMPDIR=/tmp/ccache_temp
-THIS_DIR="$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)"
 
-mkdir -p ${CCACHE_DIR} ${CCACHE_TEMPDIR}
+mkdir -p "${CCACHE_DIR}" "${CCACHE_TEMPDIR}"
 
-# add specific build scripts for each package here
-# ${THIS_DIR}/build_livox_driver.sh
-
-pushd ${HOME}/ros2_ws
+pushd "${HOME}/ros2_ws"
 
 colcon build \
   --symlink-install \
-  --packages-skip livox_ros_driver2 \
   --cmake-args \
   -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_C_COMPILER_LAUNCHER=ccache \
